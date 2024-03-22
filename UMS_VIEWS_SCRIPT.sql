8b28318b9f67a3a9e4ec5b28b2c1797535884d46
ChakradharAbhinay <145234036+ChakradharAbhinay@users.noreply.github.com>
2024-03-21 21:00:54 -0400

    UMS_VIEWS_SCRIPT.sql created views(grade_overview,professor_teaching_schedule_view,full_course_detail_view)

--- UMS_VIEWS_SCRIPT.sql
+++ UMS_VIEWS_SCRIPT.sql
@@ -77,5 +77,71 @@ JOIN
 GROUP BY 
     term.name;
 
+CREATE VIEW grade_overview AS
+SELECT
+    sc.id AS student_course_id,
+    s.id AS student_id,
+    s.first_name || ' ' || s.last_name AS student_name,
+    c.id AS course_id,
+    c.course_catalog_id,
+    c.crn,
+    g.name AS grade_name,
+    sc.percentage
+FROM
+    ums.student_course sc
+JOIN
+    ums.student s ON sc.student_id = s.id
+JOIN
+    ums.course c ON sc.course_id = c.id
+LEFT JOIN
+    ums.grade g ON sc.grade_id = g.id;
+
+
+CREATE VIEW professor_teaching_schedule_view AS
+SELECT
+    cs.id AS course_schedule_id,
+    cc.course_name,
+    t.name AS term_name,
+    cs.day,
+    TO_CHAR(cs.start_time, 'HH24:MI') AS start_time,
+    TO_CHAR(cs.end_time, 'HH24:MI') AS end_time,
+    l.building_id,
+    l.floor_id,
+    l.room_no,
+    p.first_name || ' ' || p.last_name AS professor_name
+FROM
+    ums.course_schedule cs
+JOIN
+    ums.course c ON cs.course_id = c.id
+JOIN
+    ums.term t ON c.term_id = t.id
+JOIN
+    ums.location l ON cs.location_id = l.id
+JOIN
+    ums.professor p ON c.professor_id = p.id
+JOIN
+    ums.course_catalog cc ON c.course_catalog_id = cc.id;
+
+
+CREATE OR REPLACE VIEW full_course_detail_view AS
+SELECT
+    location.id AS location_id,
+    building.name AS building,
+    floor.name AS floor,
+    location.room_no AS room_no,
+    location_type.name AS location_type,
+    course_catalog.course_name AS course_name,
+    professor.first_name || ' ' || professor.last_name AS professor_name
+FROM
+    location
+JOIN building ON building.id = location.building_id
+JOIN floor ON floor.id = location.floor_id
+JOIN location_type ON location_type.id = location.location_type_id
+JOIN course_schedule ON course_schedule.location_id = location.id
+JOIN course ON course_schedule.course_id = course.id
+JOIN course_catalog ON course.course_catalog_id = course_catalog.id
+JOIN professor ON professor.id = course.professor_id;
+
+
 
 
